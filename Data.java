335c41ddb8a5f667fce718a9a5a75ab13838d279
Alwxiejade <alexiejade99@gmail.com>
2017-05-01 14:02:08 -0600

    yayy

---
+++ Data.java
@@ -0,0 +1,21 @@
+
+public class Data {
+	
+	
+	private String engineType;
+	private int engineNumber;
+	private String engineLocation;
+	public void setEngineType(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setEngineNumber(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setEngineLocation(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
